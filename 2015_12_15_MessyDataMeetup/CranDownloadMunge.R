25cd8506bbba63209a059b74bd371510ba68bd1a
Matt Dowle <mattjdowle@gmail.com>
2015-12-17 18:00:08 -0800

    Removed non relevant from script

--- 2015_12_15_MessyDataMeetup/CranDownloadMunge.R
+++ 2015_12_15_MessyDataMeetup/CranDownloadMunge.R
@@ -3,7 +3,6 @@ cd ~/cran-mirror
 R
 require(data.table)
 
-# That Feb 28 date
 DT = fread("2015-02-28.csv")
 DT
 DT = DT[,-c("r_version","r_arch","r_os"),with=FALSE]
@@ -29,41 +28,11 @@ ans2[package %in% c("data.table","dplyr")]
 merge(ans1,ans2,by="package",all=TRUE)
 merge(ans1,ans2,by="package",all=TRUE)[order(-abs(rank.x-rank.y))]
 
-
 for (m in sprintf("2015-%02d",1:11)) {
   DT = rbindlist(lapply(dir(patt=m), fread))
   ans2 = DT[,.N,by=.(date,ip_id,package)][,.N,by=package][order(-N)][,rank:=.I]
   cat(m,"\n")
   print(ans2[package %in% c("data.table","dplyr")])
 }
-
-# END
-
-
-merge(ans1,ans2,by="package",all=TRUE)[head(order(rank.x),110)]
-
-How much does it change for August 2015?
-DT = rbindlist(lapply(dir(patt="2015-08"),fread))
-ans1 = DT[,.N,by=.(package)][order(-N)][,rank:=.I]
-ans1[package %in% c("data.table","dplyr")]
-
-ans2 = DT[,.N,by=.(ip_id,date,package)][,.N,by=package][order(-N)][,rank:=.I]
-ans2[package %in% c("data.table","dplyr")]
-merge(ans1,ans2,by="package",all=TRUE)[head(order(rank.x),110)][order(-abs(rank.y-rank.x))]
-
-
-
-
-# Adjusted ...
-ans1 = DT[,.N,by=.(package)][order(-N)][,rank:=.I]
-ans1[package %in% c("data.table","dplyr")]
-
-ans2 = DT[,.N,by=.(ip_id,package)][,.N,by=package][order(-N)][,rank:=.I]
-ans2[package %in% c("data.table","dplyr")]
-merge(ans1,ans2,by="package",all=TRUE)[head(order(rank.x),110)]
-
-DT[,.N,by=ip_id][order(N)]
-merge(ans1,ans2,by="package",all=TRUE)[head(order(rank.x),110)][order(-abs(rank.y-rank.x))]
-
 
 
